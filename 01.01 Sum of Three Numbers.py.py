35aed27c9013c43cfbc7b453c584e029c9af55e4
mpodila <mpodila@ualr.edu>
2022-08-24 14:44:20 +0000

    Sum of no

---
+++ 01.01 Sum of Three Numbers.py.py
@@ -0,0 +1,10 @@
+# Prompt for first number
+a = input("First Number: ")
+# Prompt for second number
+b = input("Second Number: ")
+# Prompt for third number
+c = input("Third Number: ")
+# Convert input to integer and add them up
+d = int(a) + int(b) + int(c)
+# Print sum
+print(d)
